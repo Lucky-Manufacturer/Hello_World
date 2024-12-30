aa08aed7193106af1caa9965d7455c00417c9e74
Alex <junk.letdown197@passinbox.com>
2024-12-30 01:18:13 -0600

    update

--- Simple Tip Calculator.py
+++ Simple Tip Calculator.py
@@ -1,7 +1,5 @@
 price = float(input("Total price: "))
 tip = float(input("What % of tip would you like to leave? "))
 
-#if "%" in tip:
-#    tip = tip.replace ("%"," ")
-
-print((price / 100) * tip)
+print("Total tip amount: " + str((price / 100) * tip))
+print("Total :" + str(price + tip))
